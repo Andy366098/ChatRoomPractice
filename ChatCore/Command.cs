4b8d066cfe78f63f5bdbf88c8881830b0538dd53
AZ366098 <84701052+Andy366098@users.noreply.github.com>
2021-12-03 21:30:56 +0800

    正要從Step08開始打到10

---
+++ ChatCore/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatCore
+{
+    class Command
+    {
+        public enum Type
+        {
+            LOGIN = 1,
+            MESSAGE
+        }
+        
+    }
+}
