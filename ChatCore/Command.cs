adafe43ba8e30f9c7c66e8e169e8cf981d435363
AZ366098 <84701052+Andy366098@users.noreply.github.com>
2021-12-07 16:16:40 +0800

    Command打到一半

--- ChatCore/Command.cs
+++ ChatCore/Command.cs
@@ -13,6 +13,50 @@ namespace ChatCore
             LOGIN = 1,
             MESSAGE
         }
-        
+        private int m_Command;
+        public int CommandID => m_Command;
+
+        private int m_Length;
+
+        private byte[] m_PacketBuffer = new byte[1024];
+        private int m_BeginPos;
+        private int m_Pos;
+
+        protected Command(int command)
+        {
+            m_Command = command;
+        }
+        public static void FetchHeader(out int length, out int command, byte[] packetData, int beginPos)
+        {
+            var header = new Command(0);
+            //中間還有兩行沒打
+            length = header.m_Length;
+            command = header.m_Command;
+        }
+        public virtual void Serialize()
+        {
+            m_Pos = m_BeginPos;
+            _WriteToBuffer(m_Length);
+            _WriteToBuffer(m_Command);
+
+        }
+        protected bool _WriteToBuffer(int i)
+        {
+            // convert int to byte array
+            var bytes = BitConverter.GetBytes(i);
+            _WriteToBuffer(bytes);
+            return true;
+        }
+        private void _WriteToBuffer(byte[] byteData)
+        {
+            // converter little-endian to network's big-endian
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(byteData);
+            }
+
+            byteData.CopyTo(m_PacketBuffer, m_BeginPos + m_Pos);
+            m_Pos += byteData.Length;
+        }
     }
 }
